ecc7108432093ab0e0d822901b9918cdba9c6c63
Brooke Hopkins <brooke.hopkins@nyu.edu>
2015-10-20 12:51:33 -0400

    Homework Week7

---
+++ Image filter/js/main.js
@@ -0,0 +1,10 @@
+
+Caman("#filteredImage", "path/to/image.jpg", function () {
+  this.brightness(5).render();
+  this.contrast()
+  this.sepia(50).render();
+});
+
+Caman("#canvas-id", function () {
+
+});
